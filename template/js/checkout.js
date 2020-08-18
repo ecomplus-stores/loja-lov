06515d928d6d36e971cd509ab72dbc5f7562029f
Carlos <61259268+lojalov@users.noreply.github.com>
2020-08-18 13:38:41 -0300

    Update checkout.js

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -1,5 +1,5 @@
 import '#template/js/checkout'
 import './custom-js/checkout'
 
-window.ecomPaymentApps = [108091, 1251, 111223]
+window.ecomPaymentApps = [108091, 1251, 1245, 111223]
 window.ecomShippingApps = [1253, 1236]
