b2371d8d41b75e8517f54e268438b7d9be064775
Carlos <61259268+lojalov@users.noreply.github.com>
2021-10-19 10:37:31 -0300

    Update checkout.js

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -1,1 +1,12 @@
 // Add your custom JavaScript for checkout here.
+// Inserir no custom-js/checkout.js
+function changeCartPageTitle(title) {
+    const router = window.storefrontApp && window.storefrontApp.router 
+    const currentRoute = router.currentRoute;
+    if(currentRoute.name == 'cart') {
+        document.title = title;
+    }
+};
+window.onload = () => {
+    changeCartPageTitle('Minha Sacola');
+}
