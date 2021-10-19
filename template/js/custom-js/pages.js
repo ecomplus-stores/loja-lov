200b016e4e942b44a3de7adf911166b2a25c60af
Carlos <61259268+lojalov@users.noreply.github.com>
2021-10-19 11:55:24 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,10 +1,1 @@
 // Add your custom JavaScript for storefront pages here.
-// Inserir no custom-js/pages.js
-function changeCartButtonTitle(title) {
-    const $cartButton = document.querySelector("#cart-button");
-    $cartButton.title = title;
-}
-
-window.onload = () => {
-    changeCartButtonTitle('Abrir Sacola');
-}
