2b2e91499cf1e71a8fd6f95b0a7d5fa6433d87a7
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-11-27 23:23:22 -0300

    chore: create offer page

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,19 @@
 // Add your custom JavaScript for storefront pages here.
+if (window.location.pathname === '/promocao') {
+    const timestamp = Date.now()
+    EcomSearch.dslMiddlewares.push((dsl) => {
+      dsl.query.bool.filter.push({
+        script: {
+          script: {
+            lang: 'painless',
+            source: "doc['price'].value > 0 && doc['base_price'].value > 0" +
+              " && (doc['price_effective_date.start'].empty || " +
+                `doc['price_effective_date.start'].date.millis <= ${timestamp}L)` +
+              " && (doc['price_effective_date.end'].empty || " +
+                `doc['price_effective_date.end'].date.millis >= ${timestamp}L)` +
+              " ? doc['base_price'].value > doc['price'].value : false"
+          }
+        }
+      })
+    })
+  }
